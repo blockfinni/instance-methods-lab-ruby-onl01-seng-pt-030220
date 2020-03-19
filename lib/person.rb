a69bb8d75247c73d92ad51df3d2b7596023f2fdb
Benjamin A Ajisefinni Jr <aj.no.limit.1@gmail.com>
2020-03-19 03:41:12 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,14 @@
+class Person 
+  
+  #method for bark 
+  def bark
+    puts "Woof!" 
+  end #method end 
+  
+  #second method 
+  def sit 
+    puts "The Dog is sitting"
+  end #method end 
+  
+  
+end #class end 
