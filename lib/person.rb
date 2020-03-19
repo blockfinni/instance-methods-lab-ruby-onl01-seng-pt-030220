b418b234ef066a64741b17ebe8281bd275baa2dc
Benjamin A Ajisefinni Jr <aj.no.limit.1@gmail.com>
2020-03-19 03:42:30 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,14 @@
+class Person 
+  
+  #method for bark 
+  def talk
+    puts "Hello World!" 
+  end #method end 
+  
+  #second method 
+  def walk
+    puts "The Person is walking"
+  end #method end 
+  
+  
+end #class end 
